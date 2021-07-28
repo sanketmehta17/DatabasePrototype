cca98bde3000fca95db8b668f682c871e9dfdc29
VENKATA SAIKIRAN KATTEKOLA <vn857734@dal.ca>
2021-07-28 21:17:18 +0530

    Login and MFA part

--- src/main/java/dbms/Main.java
+++ src/main/java/dbms/Main.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String args[]) throws IOException {
+        ValidateUser user = new ValidateUser();
+        Boolean isUserValidUser = user.performUserValidation();
+        if(isUserValidUser) {
+            System.out.println(">>>You are signed in as:"+user.getUserName());
+        } else {
+            System.out.println(">> Error in login <<");
+        }
         Datasource datasource = new Datasource();
         datasource.connect();
         Database db = datasource.addDatabase("Test");

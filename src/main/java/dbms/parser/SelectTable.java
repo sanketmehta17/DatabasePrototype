5e18389ce215061be9388e1efaff65b6071a6318
VENKATA SAIKIRAN KATTEKOLA <vn857734@dal.ca>
2021-08-02 04:57:26 +0530

    5408 Project - Select feature

--- src/main/java/dbms/parser/SelectTable.java
+++ src/main/java/dbms/parser/SelectTable.java
@@ -1,23 +1,53 @@
 package dbms.parser;
 
+import dbms.Database;
 import dbms.Datasource;
+import dbms.Table;
+import dbms.metadata.TableMeta;
 
+import java.util.Arrays;
+import java.util.Set;
 import java.util.regex.Matcher;
 
-public class SelectTable extends Parser
-{
+public class SelectTable extends Parser {
 
-    public SelectTable(Datasource datasource) {
-        super(datasource);
+
+  public SelectTable(Datasource datasource) {
+    super(datasource);
+  }
+
+  @Override
+  protected String getRegex() {
+    return "select *(.*) *from *(.*) *(where *(.*))*";
+  }
+
+  @Override
+  protected void execute(Matcher matcher, String query) {
+    Database currentDatabase = datasource.getCurrentDatabase();
+    String tableName = matcher.group(2).trim();
+    String conditions [] = new String[2];
+    if(tableName.contains("where")) {
+      conditions[0] = tableName.substring(tableName.indexOf("where")+5).trim();
+      tableName = tableName.substring(0,tableName.indexOf("where")-1);
     }
-
-    @Override
-    protected String getRegex() {
-        return null;
+    Table table = currentDatabase.getTable(tableName);
+    TableMeta tableMeta = table.getMetaData();
+    Set<String> tableColumns = tableMeta.getColumns();
+    System.out.println("**** Printing table :" + tableName + "*****");
+    if (matcher.group(1).trim().contains("*") && !matcher.group(2).trim().contains("where")) {
+      //select all
+      for (String key : tableColumns) {
+        System.out.print(key + "  ");
+      }
+      System.out.println("\n" + table.toDBString().replace("~~", "  "));
+    } else if (matcher.group(2).trim().contains("where")) {
+      //print with where condition
+      String keyValue[] = conditions[0].split("=");
+      String key = keyValue[0];
+      String value = keyValue[1];
+      System.out.println("**** Printing the values for the key&value pair*****"+key +" "+value);
     }
+    System.out.println("**** End of printing the table *****");
+  }
 
-    @Override
-    protected void execute(Matcher matcher, String query) {
-
-    }
 }

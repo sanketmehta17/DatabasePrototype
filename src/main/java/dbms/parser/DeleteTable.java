40acaa63c89541a3980489b5cb7a3e8f70a442f6
sumehta <sn630454@dal.ca>
2021-08-01 10:56:19 -0300

    modified DeleteTable file

--- src/main/java/dbms/parser/DeleteTable.java
+++ src/main/java/dbms/parser/DeleteTable.java
@@ -1,14 +1,8 @@
 package dbms.parser;
 
 import dbms.*;
-import dbms.metadata.ColumnMeta;
-import dbms.metadata.TableMeta;
-
-import java.util.*;
 import java.util.regex.Matcher;
 
-import static dbms.Constants.escapeSequence;
-import static dbms.Constants.secondaryDelimiter;
 import static java.util.Objects.isNull;
 
 public class DeleteTable extends Parser{
@@ -18,8 +12,7 @@ public class DeleteTable extends Parser{
 
   @Override
   protected String getRegex() {
-    return "delete fr1" +
-            "om +(.+)";
+    return "delete from +(.+)";
   }
 
   private void checkTableExist(Table table) throws Exception {
@@ -27,20 +20,6 @@ public class DeleteTable extends Parser{
       throw new Exception("Invalid table");
     }
   }
-
-  private Map<String, Object> getInputMap(String columnNamesString, String columnValuesString) throws Exception {
-    List<String> columnsNames = List.of(columnNamesString.split(escapeSequence + secondaryDelimiter));
-    List<String> columnValues = List.of(columnValuesString.split(escapeSequence + secondaryDelimiter));
-    if(columnsNames.size() != columnValues.size()) {
-      throw new Exception("column and values does not match");
-    }
-    Map<String, Object> inputMap = new HashMap<>();
-    for(int i=0; i<columnsNames.size();i++) {
-      inputMap.put(columnsNames.get(i).trim(), columnValues.get(i).trim());
-    }
-    return inputMap;
-  }
-
 
   @Override
   protected void execute(Matcher matcher, String query) {
@@ -53,7 +32,7 @@ public class DeleteTable extends Parser{
       table.deleteContent();
 
     } catch (Exception ex) {
-      System.out.println("Invalid query for insert");
+      System.out.println("Invalid query for delete");
     }
   }
 }
